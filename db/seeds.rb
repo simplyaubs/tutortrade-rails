1671d1f24307173838b154f921aa2183f41d4beb
Aubrey Howell <simplyaubs@gmail.com>
2013-11-20 11:45:16 -0700

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create! \
+	first_name: "Madelaine",
+	last_name: "Mackey",
+	year: "Junior",
+	gender: "female",
+	age: 21,
+	subject: "Math",
+	tutor_pic: "madelaine.jpg",
+	major: "Applied Mathmatics",
+	bio: "I was introduced to math at an early age. After 1 + 1 = 2, I was hooked. Transcendental Numbers, Multiplicative Property of Equality, and Venn Diagrams make me happy."
+
+	User.create! \
+	first_name: "Wilmer",
+	last_name: "Waugh",
+	year: "Sophmore",
+	gender: "male",
+	age: 20,
+	subject: "English",
+	tutor_pic: "wilmer.jpg",
+	major: "English and Medieval Literature",
+	bio: "All throughout childhood, you would catch me with my nose in a book. I'm passionate about proper grammar and secular works of the Renaissance era."
+
+	User.create! \
+	first_name: "Chante",
+	last_name: "Calvo",
+	year: "Senior",
+	gender: "female",
+	age: 22,
+	subject: "Science",
+	tutor_pic: "chante.jpg",
+	major: "Cellular Biology",
+	bio: "Understanding cells in terms of their molecular components is a very important skill. Really, I think everyone should be able to their organelles and interactions with their environment."
+
+	User.create! \
+	first_name: "Frankie",
+	last_name: "Faux",
+	year: "Freshman",
+	gender: "male",
+	age: 19,
+	subject: "History",
+	tutor_pic: "frankie.jpg",
+	major: "History - Concentration in European History",
+	bio: "Abraham Lincoln was great and all, but Alexander the Great was far superior! The period known as classical antiquity began with the rise of the city-states of Ancient Greece and was amazing!"
